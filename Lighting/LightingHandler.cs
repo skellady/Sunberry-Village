82075d020375bc0fa1c2cbd302ada7d0fafe3fdd
sophiesalacia <sophiesalacia@users.noreply.github.com>
2023-09-03 02:07:28 -0400

    Removed extraneous method and some comments

--- Lighting/LightingHandler.cs
+++ Lighting/LightingHandler.cs
@@ -1,24 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using StardewValley;
-using SunberryVillage.Utilities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SunberryVillage.Lighting
 {
-	// storing these for later while i completely fucking overhaul this goddamn code
-	//private const float DefaultRadius = 4.8f;
-	//private static readonly Vector2 DefaultPosition = new Vector2(59f, 85.5f) * 64f; // the center of the fountain is (58, 86) - shifting it up and to the right a bit to better cover everyone
-
-	// Overcomplicated this but if i need to add more lights in the future, it will be pretty trivial, so that's nice
-	// Consider converting to CP asset for ease of use by max and other sbv resident authors
-	// you know, if you want to overcomplicate things even further...
-
 	internal static class LightingHandler
 	{
-		internal static Dictionary<string, LightDataModel> Lights = new();				// contains lights defined in asset "SunberryTeam.SBV/Lights"
-		internal static readonly Dictionary<string, LightDataModel> TempLights = new();	// contains lights defined via console command - these DO NOT persist through save/load
+		internal static Dictionary<string, LightDataModel> Lights = new();              // contains lights defined in asset "SunberryTeam.SBV/Lights"
+		internal static readonly Dictionary<string, LightDataModel> TempLights = new(); // contains lights defined via console command - these DO NOT persist through save/load
 
 		internal const string LightsAssetPath = "SunberryTeam.SBV/Lights";
 
@@ -59,34 +49,11 @@ namespace SunberryVillage.Lighting
 			return false;
 		}
 
-		internal static bool CreateDefaultLight(out string id, out string error)
-		{
-			error = "";
-			id = "";
-
-			string tempId = Utils.GenerateRandomString(6);
-			GameLocation loc = Game1.currentLocation;
-			Vector2 pos = Game1.player.getTileLocation();
-			float intensity = 1f;
-
-			LightDataModel lightData = new(loc, pos, intensity);
-			if (!TempLights.TryAdd(tempId, lightData))
-			{
-				error = $"Unable to add light with parameters {{\"{tempId}\", \"{pos.X}\", \"{pos.Y}\", \"{intensity}\"}}.";
-			}
-
-			if (error != "")
-				return false;
-
-			id = tempId;
-			return true;
-		}
-
 		internal static bool RemoveTempLight(string id)
 		{
 			if (!TempLights.ContainsKey(id))
 				return false;
-			
+
 			LightDataModel lightData = TempLights[id];
 			Game1.currentLightSources?.Remove(lightData.LightSource);
 			TempLights.Remove(id);
