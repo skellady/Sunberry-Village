3e3156a1015f6acb1e824a3d98af7f4e642b8a85
sophiesalacia <sophiesalacia@users.noreply.github.com>
2024-01-07 12:11:11 -0500

    Changed logging of trace- and debug-level messages

--- Code/Utilities/Log.cs
+++ Code/Utilities/Log.cs
@@ -10,15 +10,17 @@ internal class Log
 
 	public static void Verbose(object obj) => Monitor.VerboseLog(obj.ToString() ?? string.Empty);
 
-	// Only log Trace messages if compiled in Debug mode.
-	public static void Trace(object obj)
-	{
-#if DEBUG
-		Monitor.Log(obj.ToString() ?? string.Empty);
-#endif
-	}
+	public static void Trace(object obj) => Monitor.Log(obj.ToString() ?? string.Empty);
 
-	public static void Debug(object obj) => Monitor.Log(obj.ToString() ?? string.Empty, LogLevel.Debug);
+	// Only log Debug messages if compiled in Debug mode.
+	public static void Debug(object obj)
+	{
+		#if DEBUG
+
+		Monitor.Log(obj.ToString() ?? string.Empty, LogLevel.Debug);
+
+		#endif
+	}
 
 	public static void Info(object obj) => Monitor.Log(obj.ToString() ?? string.Empty, LogLevel.Info);
 
