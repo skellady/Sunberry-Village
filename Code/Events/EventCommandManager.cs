096a9c57838d8800fcdaea5c40d11ced5cf8bff7
sophiesalacia <sophiesalacia@users.noreply.github.com>
2024-07-02 18:28:41 -0400

    Added wizard warp stuff :3

--- Code/Events/EventCommandManager.cs
+++ Code/Events/EventCommandManager.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.Audio;
 using SunberryVillage.Events.Phone;
 using SunberryVillage.Events.Tarot;
 using SunberryVillage.Utilities;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace SunberryVillage.Events;
 internal class EventCommandManager
@@ -46,111 +52,370 @@ internal class EventCommandManager
 
 			if (!WizardWarpInitialized)
 			{
-				warpHelper = new (ev, args, eventContext);
+				warpHelper = new WizardWarpHelper(ev, args, eventContext);
+
+				if (warpHelper.HasError)
+				{
+					ev.CurrentCommand++;
+					return;
+				}
+
 				WizardWarpInitialized = true;
 			}
 
-			if (warpHelper.Update())
-			{
-				WizardWarpInitialized = false;
-				ev.CurrentCommand++;
+			if (!warpHelper.Update())
 				return;
-			}
-			else
-			{
-				return;
-			}
 
-			//eventContext.Location.TemporarySprites.Add(
-			//	new TemporaryAnimatedSprite(
-			//		textureName: "LooseSprites\\Cursors",
-			//		sourceRect: new Rectangle(387, 1965, 16, 31),
-			//		animationInterval: 9999f,
-			//		animationLength: 1,
-			//		numberOfLoops: 999999,
-			//		position: new Vector2(xVal, yVal) * 64f + new Vector2(0f, 4f),
-			//		flicker: false,
-			//		flipped: false,
-			//		layerDepth: 1f,
-			//		alphaFade: 0f,
-			//		color: Color.White,
-			//		scale: 4f,
-			//		scaleChange: 0f,
-			//		rotation: 0f,
-			//		rotationChange: 0f)
-			//	{
-			//		motion = new Vector2(2f, -2f),
-			//		acceleration = new Vector2(0.1f, 0f),
-			//		scaleChange = -0.02f,
-			//		alphaFade = 0.001f
-			//	});
+			WizardWarpInitialized = false;
+			ev.CurrentCommand++;
 		});
 	}
 
-	private class WizardWarpHelper
+	internal class WizardWarpHelper
 	{
-		int xVal = 0;
-		int yVal = 0;
+		internal bool HasError;
 
-		int counter = 0;
+		private readonly Event _ev;
+		private string[] _args;
+		private readonly EventContext _eventContext;
 
-		TemporaryAnimatedSprite warp;
+		private enum AnimState
+		{
+			Warping,
+			Landing,
+			Landed,
+			Finished
+		}
+		private AnimState _state;
+
+		private readonly Vector2 _targetPos;
+		private int _timer;
+		private int _sparkleTimer;
+		private int _errorTimer;
+		private bool _linedUp;
+
+		private readonly List<TemporaryAnimatedSprite> _warpingSprites = new();
+		private TemporaryAnimatedSprite _landingSprite;
+		private TemporaryAnimatedSprite _landedSprite;
+		private TemporaryAnimatedSprite _finishedSprite;
 
 		public WizardWarpHelper(Event ev, string[] args, EventContext eventContext)
 		{
-			if (args.Length < 3)
-			{
-				ev.LogCommandError(args, "Not enough arguments supplied", false);
-			}
-			else
-			{
-				bool xParsed = int.TryParse(args[1], out xVal);
-				bool yParsed = int.TryParse(args[2], out yVal);
+			_ev = ev;
+			_args = args;
+			_eventContext = eventContext;
 
-				if (!xParsed || !yParsed)
+			if (!ArgUtility.HasIndex(args, 2))
+			{
+				ev.LogCommandError(args, "Not enough arguments supplied");
+				HasError = true;
+				return;
+			}
+
+			if (!ArgUtility.TryGetInt(args, 1, out int xPos, out string error) || !ArgUtility.TryGetInt(args, 2, out int yPos, out error))
+			{
+				ev.LogCommandError(args, error);
+				HasError = true;
+				return;
+			}
+			
+			//if (!ArgUtility.TryGetOptionalDirection(args, 3, out int direction, out error, 2))
+			//{
+			//	ev.LogCommandError(args, error);
+			//	HasError = true;
+			//	return;
+			//}
+
+			// formula for initial velocity given distance, time, and final velocity:
+			// u = (2 * s / t) - v where:
+			// - u is the initial velocity
+			// - s is the distance
+			// - t is the time
+			// - v is the final velocity
+
+			// formula for deleceration given distance, initial and final velocity:
+			// a = (v^2 - u^2) / (2s) where:
+			// - a is the deceleration
+			// - v is the final velocity
+			// - u is the initial velocity
+			// - s is the distance over which the deceleration occurs
+
+			Point targetTile = new(xPos, yPos - 1);
+			_targetPos = targetTile.ToVector2() * 64f + new Vector2(4f, -16f); // i'm not gonna pretend to understand why this offset is needed. fucking npc drawing code
+
+			int numberOfWarpClones = Utils.Random.Next(6, 10);
+			const int numberOfTrails = 3;
+			int initialRotationDeg = Utils.Random.Next(360);
+			float rotationDeg = 360f / numberOfWarpClones;
+			const float time = 150f;
+			const float alphaFade = -1f / time;
+			int delay = 1000 / numberOfWarpClones;
+
+			Vector2 distanceVector = new Vector2(12f, 0f) * 64f;
+
+			for (int i = 0; i < numberOfWarpClones; i++)
+			{
+				Vector2 offset = -distanceVector.RotateDegrees(initialRotationDeg + i * rotationDeg);
+				Vector2 initialPos = _targetPos - offset;
+				Vector2 finalVelocity = Vector2.Zero;
+				Vector2 initialVelocity = new(2 * offset.X / time, 2 * offset.Y / time);
+				
+				float accelX = (float) ((Math.Pow(finalVelocity.X, 2) - Math.Pow(initialVelocity.X, 2)) / (2 * offset.X));
+				float accelY = (float) ((Math.Pow(finalVelocity.Y, 2) - Math.Pow(initialVelocity.Y, 2)) / (2 * offset.Y));
+				
+				Vector2 accel = new(accelX, accelY);
+
+				for (int j = 0; j < numberOfTrails; j++)
 				{
-					ev.LogCommandError(args, "One or more arguments not provided in correct format", false);
+					_warpingSprites.Add(new TemporaryAnimatedSprite(
+						textureName: "Characters/Wizard",
+						sourceRect: new Rectangle(32, 160, 16, 32),
+						position: initialPos,
+						false,
+						0f,
+						Color.Lerp(Color.White, Color.MediumPurple, 0.25f * j)
+					)
+					{
+						scale = 4f,
+						motion = initialVelocity,
+						alpha = 0f,
+						alphaFade = alphaFade /*+ (0.001f * j)*/,
+						drawAboveAlwaysFront = true,
+						stopAcceleratingWhenVelocityIsZero = true,
+						acceleration = accel,
+						usePreciseTiming = true,
+						Parent = _eventContext.Location
+					});
+
+					DelayedAction.addTemporarySpriteAfterDelay(_warpingSprites[i * numberOfTrails + j], _eventContext.Location, (i + j) * delay);
 				}
 			}
 
-			warp = new TemporaryAnimatedSprite(
-				textureName: "LooseSprites\\Cursors",
-				sourceRect: new Rectangle(387, 1965, 16, 31),
-				animationInterval: 9999f,
-				animationLength: 1,
-				numberOfLoops: 999999,
-				position: new Vector2(xVal - 10, yVal - 10) * 64f,
-				flicker: false,
-				flipped: false,
-				layerDepth: 1f,
-				alphaFade: 0f,
-				color: Color.White * 0f,
-				scale: 0f,
-				scaleChange: 0.05f,
-				rotation: 0f,
-				rotationChange: 0f
-			)
-			{
-				motion = new Vector2(2f, 2f),
-				alphaFade = -0.1f
-			};
+			Game1.playSound("wand", out ICue sound);
+			_ev.TrackSound(sound);
+
+			_state = AnimState.Warping;
 		}
 
-		internal bool Update()
+		public bool Update()
 		{
-			if (warp.Position.X / 64f >= xVal && warp.Position.Y / 64f >= yVal)
+			GameTime time = Game1.currentGameTime;
+
+			// force an error-out if command takes more than 5 seconds
+			_errorTimer += time.ElapsedGameTime.Milliseconds;
+			if (_errorTimer > 5000)
 			{
-				warp.Position = new(xVal, yVal);
-				warp.motion = Vector2.Zero;
+				HasError = true;
+				return true;
 			}
 
-			counter++;
-			
-			if (warp.color.A < 255)
-				return false;
+			switch (_state)
+			{
+				case AnimState.Warping:
+					{
+						_sparkleTimer += time.ElapsedGameTime.Milliseconds;
+						if (_sparkleTimer > 30)
+						{
+							_sparkleTimer = 0;
 
-			return true;
+							TemporaryAnimatedSprite warpSprite = _warpingSprites.GetRandomElementFromList();
 
+							_eventContext.Location.temporarySprites.Add(new TemporaryAnimatedSprite(Utils.Random.Next(10, 12), warpSprite.Position + new Vector2(Utils.Random.Next(33) - 16, Utils.Random.Next(33) - 8) * 4f, Color.MediumPurple)
+							{
+								motion = warpSprite.motion / 4f,
+								alphaFade = 0.01f,
+								layerDepth = 0.8f,
+								scale = 1f,
+								alpha = 0.85f
+							});
+						}
+
+						if (_warpingSprites[^3].motion == Vector2.Zero && _warpingSprites[^3].alpha >= 1f)
+						{
+							_linedUp = true;
+						}
+
+						if (_linedUp)
+						{
+							foreach (TemporaryAnimatedSprite sprite in _warpingSprites)
+							{
+								sprite.motion = (_targetPos - sprite.Position) / 10f;
+							}
+
+							//if (!_linedUp)
+							//{
+							//	_linedUp = true;
+							//
+							//	// rough but idc it gets the job done
+							//	// throw some sparkles around to hide how crappy this part looks lol
+							//	foreach (TemporaryAnimatedSprite sprite in _warpingSprites)
+							//	{
+							//		sprite.motion = (_targetPos - sprite.Position);
+							//		sprite.acceleration = -sprite.motion;
+							//	}
+							//}
+
+							_timer += time.ElapsedGameTime.Milliseconds;
+						}
+
+						if (_timer >= 250)
+						{
+							_timer = 0;
+
+							_landingSprite = new TemporaryAnimatedSprite(
+								textureName: "Characters/Wizard",
+								sourceRect: new Rectangle(16, 160, 16, 32),
+								position: _targetPos,
+								flipped: false,
+								alphaFade: 0f,
+								color: Color.White
+							)
+							{
+								scale = 4f,
+								Parent = _eventContext.Location
+							};
+
+							foreach(TemporaryAnimatedSprite sprite in _warpingSprites)
+								_eventContext.Location.temporarySprites.Remove(sprite);
+
+							_eventContext.Location.temporarySprites.Add(_landingSprite);
+
+							_state = AnimState.Landing;
+						}
+
+						return false;
+					}
+
+				case AnimState.Landing:
+					{
+						_sparkleTimer += time.ElapsedGameTime.Milliseconds;
+						if (_sparkleTimer > 40)
+						{
+							_sparkleTimer = 0;
+
+							_eventContext.Location.temporarySprites.Add(new TemporaryAnimatedSprite(Utils.Random.Next(10, 12), _landingSprite.Position + new Vector2(Utils.Random.Next(33) - 16, Utils.Random.Next(33) - 8) * 4f, Color.MediumPurple)
+							{
+								motion = _landingSprite.motion / 4f,
+								alphaFade = 0.01f,
+								layerDepth = 0.8f,
+								scale = 1f,
+								alpha = 0.85f
+							});
+						}
+
+						_timer += Game1.currentGameTime.ElapsedGameTime.Milliseconds;
+						
+						if (_timer >= 150)
+						{
+							_timer = 0;
+							
+							_landedSprite = new TemporaryAnimatedSprite(
+								textureName: "Characters/Wizard",
+								sourceRect: new Rectangle(0, 160, 16, 32),
+								position: _targetPos,
+								flipped: false,
+								alphaFade: 0f,
+								color: Color.White
+							)
+							{
+								scale = 4f,
+								Parent = _eventContext.Location
+							};
+
+							_eventContext.Location.temporarySprites.Remove(_landingSprite);
+							_eventContext.Location.temporarySprites.Add(_landedSprite);
+
+							_state = AnimState.Landed;
+						}
+
+						return false;
+					}
+
+				case AnimState.Landed:
+					{
+						_sparkleTimer += time.ElapsedGameTime.Milliseconds;
+						if (_sparkleTimer > 50)
+						{
+							_sparkleTimer = 0;
+							
+							_eventContext.Location.temporarySprites.Add(new TemporaryAnimatedSprite(Utils.Random.Next(10, 12), _landedSprite.Position + new Vector2(Utils.Random.Next(33) - 16, Utils.Random.Next(33) - 8) * 4f, Color.MediumPurple)
+							{
+								motion = _landedSprite.motion / 4f,
+								alphaFade = 0.01f,
+								layerDepth = 0.8f,
+								scale = 1f,
+								alpha = 0.85f
+							});
+						}
+
+						_timer += Game1.currentGameTime.ElapsedGameTime.Milliseconds;
+						
+						if (_timer >= 150)
+						{
+							_timer = 0;
+							
+							_finishedSprite = new TemporaryAnimatedSprite(
+								textureName: "Characters/Wizard",
+								sourceRect: new Rectangle(32, 128, 16, 32),
+								position: _targetPos,
+								flipped: false,
+								alphaFade: 0f,
+								color: Color.White
+							)
+							{
+								scale = 4f,
+								Parent = _eventContext.Location
+							};
+
+							_eventContext.Location.temporarySprites.Remove(_landedSprite);
+							_eventContext.Location.temporarySprites.Add(_finishedSprite);
+
+							_state = AnimState.Finished;
+						}
+
+						return false;
+					}
+
+				case AnimState.Finished:
+					{
+						_sparkleTimer += time.ElapsedGameTime.Milliseconds;
+						if (_sparkleTimer > 60)
+						{
+							_sparkleTimer = 0;
+							
+							_eventContext.Location.temporarySprites.Add(new TemporaryAnimatedSprite(Utils.Random.Next(10, 12), _finishedSprite.Position + new Vector2(Utils.Random.Next(33) - 16, Utils.Random.Next(33) - 8) * 4f, Color.MediumPurple)
+							{
+								motion = _finishedSprite.motion / 4f,
+								alphaFade = 0.01f,
+								layerDepth = 0.8f,
+								scale = 1f,
+								alpha = 0.85f
+							});
+						}
+
+						_timer += Game1.currentGameTime.ElapsedGameTime.Milliseconds;
+
+						if (_timer >= 150)
+						{
+							_eventContext.Location.temporarySprites.Remove(_finishedSprite);
+
+							NPC wizard = _ev.actors.Find(npc => npc.Name.Equals("Wizard"));
+							if (wizard is not null)
+							{
+								wizard.position.X = _ev.OffsetPositionX(_targetPos.X);
+								wizard.position.Y = _ev.OffsetPositionY(_targetPos.Y + 80f);
+								wizard.IsWalkingInSquare = false;
+							}
+
+							return true;
+						}
+						return false;
+					}
+
+				default:
+					return false;
+			}
 		}
+
 	}
 }
--- Code/Utilities/Utils.cs
+++ Code/Utilities/Utils.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using HarmonyLib;
+using Microsoft.Xna.Framework;
 
 namespace SunberryVillage.Utilities;
 
 internal class Utils
 {
-	private static readonly Random Random = new();
+	internal static readonly Random Random = new();
 
 	/// <summary>
 	/// Generates a string of random alphanumeric characters of the specified <paramref name="length"/>.
@@ -40,11 +41,11 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="instructionToCheck">The instruction to check</param>
 	/// <param name="instructionToMatch">The instruction to check against</param>
-	/// <param name="opcodeOnly">If true, check only the opcode and ignore the operand</param>
+	/// <param name="ignoreOperand">If true, check only the opcode and ignore the operand</param>
 	/// <returns>True if the instructions match, or false otherwise</returns>
-	public static bool DoesInstructionMatch(CodeInstruction instructionToCheck, CodeInstruction instructionToMatch, bool opcodeOnly = false)
+	public static bool DoesInstructionMatch(CodeInstruction instructionToCheck, CodeInstruction instructionToMatch, bool ignoreOperand = false)
 	{
-		if (opcodeOnly)
+		if (ignoreOperand)
 			return instructionToCheck.opcode.Equals(instructionToMatch.opcode);
 
 		return instructionToCheck.opcode.Equals(instructionToMatch.opcode) && (instructionToCheck.operand is Label ? instructionToMatch.operand is Label : instructionToCheck.operand?.Equals(instructionToMatch.operand) ?? instructionToMatch.operand is null);
@@ -56,14 +57,14 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="sequenceToCheck">The list of instructions to check</param>
 	/// <param name="sequenceToMatch">The list of instructions to check against</param>
-	/// <param name="opcodesOnly">If true, check only the opcodes and ignore the operands</param>
+	/// <param name="ignoreOperands">If true, check only the opcodes and ignore the operands</param>
 	/// <returns></returns>
-	public static bool DoesSequenceMatch(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool opcodesOnly = false)
+	public static bool DoesSequenceMatch(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool ignoreOperands = false)
 	{
 		if (sequenceToMatch.Count != sequenceToCheck.Count)
 			return false;
 
-		return !sequenceToMatch.Where((instruction, index) => !DoesInstructionMatch(sequenceToCheck[index], instruction, opcodesOnly)).Any();
+		return !sequenceToMatch.Where((instruction, index) => !DoesInstructionMatch(sequenceToCheck[index], instruction, ignoreOperands)).Any();
 	}
 
 	/// <summary>
@@ -71,13 +72,13 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="sequenceToCheck">The list of instructions to search</param>
 	/// <param name="sequenceToMatch">The list of instructions to try to match</param>
-	/// <param name="opcodesOnly">If true, check only the opcodes and ignore the operands</param>
+	/// <param name="ignoreOperands">If true, check only the opcodes and ignore the operands</param>
 	/// <returns>The index of the first instance of the sequence that matches the provided sequence, or -1 if no such sequence is found</returns>
-	public static int FindFirstSequenceMatch(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool opcodesOnly = false)
+	public static int FindFirstSequenceMatch(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool ignoreOperands = false)
 	{
 		for (int index = 0; index <= sequenceToCheck.Count - sequenceToMatch.Count; index++)
 		{
-			if (DoesSequenceMatch(sequenceToCheck.GetRange(index, sequenceToMatch.Count), sequenceToMatch, opcodesOnly))
+			if (DoesSequenceMatch(sequenceToCheck.GetRange(index, sequenceToMatch.Count), sequenceToMatch, ignoreOperands))
 				return index;
 		}
 
@@ -89,15 +90,15 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="sequenceToCheck">The list of instructions to search</param>
 	/// <param name="sequenceToMatch">The list of instructions to try to match</param>
-	/// <param name="opcodesOnly">If true, check only the opcodes and ignore the operands</param>
+	/// <param name="ignoreOperands">If true, check only the opcodes and ignore the operands</param>
 	/// <returns>The indices of sequences that match the provided sequence, or -1 if no such sequences are found</returns>
-	public static List<int> FindAllSequenceMatches(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool opcodesOnly = false)
+	public static List<int> FindAllSequenceMatches(List<CodeInstruction> sequenceToCheck, List<CodeInstruction> sequenceToMatch, bool ignoreOperands = false)
 	{
 		List<int> matchIndices = new();
 
 		for (int index = 0; index <= sequenceToCheck.Count - sequenceToMatch.Count; index++)
 		{
-			if (DoesSequenceMatch(sequenceToCheck.GetRange(index, sequenceToMatch.Count), sequenceToMatch, opcodesOnly))
+			if (DoesSequenceMatch(sequenceToCheck.GetRange(index, sequenceToMatch.Count), sequenceToMatch, ignoreOperands))
 				matchIndices.Add(index);
 		}
 
@@ -109,11 +110,11 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="sequenceToSearch">The list of instructions to search</param>
 	/// <param name="instructionToFind">The instruction to search for</param>
-	/// <param name="opcodeOnly">If true, check only the opcode and ignore the operand</param>
+	/// <param name="ignoreOperand">If true, check only the opcode and ignore the operand</param>
 	/// <returns>The index of the first instance of the provided CodeInstruction, or -1 if no such instruction is found</returns>
-	public static int FindFirstIndex(List<CodeInstruction> sequenceToSearch, CodeInstruction instructionToFind, bool opcodeOnly = false)
+	public static int FindFirstIndex(List<CodeInstruction> sequenceToSearch, CodeInstruction instructionToFind, bool ignoreOperand = false)
 	{
-		return sequenceToSearch.FindIndex(inst => DoesInstructionMatch(inst, instructionToFind, opcodeOnly));
+		return sequenceToSearch.FindIndex(inst => DoesInstructionMatch(inst, instructionToFind, ignoreOperand));
 	}
 
 	/// <summary>
@@ -121,10 +122,27 @@ internal static class HarmonyUtils
 	/// </summary>
 	/// <param name="sequenceToSearch">The list of instructions to search</param>
 	/// <param name="instructionToFind">The instruction to search for</param>
-	/// <param name="opcodeOnly">If true, check only the opcode and ignore the operand</param>
+	/// <param name="ignoreOperand">If true, check only the opcode and ignore the operand</param>
 	/// <returns>The first instance of the provided CodeInstruction, or null if no such instruction is found</returns>
-	public static CodeInstruction FindFirst(List<CodeInstruction> sequenceToSearch, CodeInstruction instructionToFind, bool opcodeOnly = false)
+	public static CodeInstruction FindFirst(List<CodeInstruction> sequenceToSearch, CodeInstruction instructionToFind, bool ignoreOperand = false)
 	{
-		return sequenceToSearch.Find(inst => DoesInstructionMatch(inst, instructionToFind, opcodeOnly));
+		return sequenceToSearch.Find(inst => DoesInstructionMatch(inst, instructionToFind, ignoreOperand));
+	}
+}
+
+public static class Vector2Extensions
+{
+	private const float DegToRad = (float)Math.PI/180;
+
+	public static Vector2 RotateDegrees(this Vector2 v, float degrees)
+	{
+		return v.RotateRadians(degrees * DegToRad);
+	}
+
+	public static Vector2 RotateRadians(this Vector2 v, float radians)
+	{
+		float ca = (float)Math.Cos(radians);
+		float sa = (float)Math.Sin(radians);
+		return new Vector2(ca * v.X - sa * v.Y, sa * v.X + ca * v.Y);
 	}
 }
