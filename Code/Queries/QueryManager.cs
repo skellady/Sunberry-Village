188006b3bdf8504a9a742c9aad327430cda15128
sophiesalacia <sophiesalacia@users.noreply.github.com>
2024-08-18 10:03:54 -0400

    added GSQ to check tomorrow's weather ☁

--- Code/Queries/QueryManager.cs
+++ Code/Queries/QueryManager.cs
@@ -13,8 +13,8 @@ internal class QueryManager
 	private static void RegisterQueriesAndPreconditions(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
 	{
 		ItemQueryResolver.Register("SunberryTeam.SBVSMAPI_ChooseNFromSet", ChooseNFromSetQuery.HandleChooseNFromSetQuery);
+		GameStateQuery.Register("SunberryTeam.SBVSMAPI_WeatherTomorrow", WeatherTomorrowQuery.HandleWeatherTomorrowQuery);
 		Event.RegisterPrecondition("SunberryTeam.SBVSMAPI_IsPassiveFestivalToday", IsPassiveFestivalPrecondition.IsPassiveFestivalToday);
 		Event.RegisterPrecondition("SunberryTeam.SBVSMAPI_IsPassiveFestivalOnGivenDay", IsPassiveFestivalPrecondition.IsPassiveFestivalOnGivenDay);
 	}
-
 }
---
+++ Code/Queries/WeatherTomorrowQuery.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using StardewValley;
+using StardewValley.Delegates;
+using GSQHelper = StardewValley.GameStateQuery.Helpers;
+
+namespace SunberryVillage.Queries;
+
+internal class WeatherTomorrowQuery
+{
+	
+	internal static bool HandleWeatherTomorrowQuery(string[] query, GameStateQueryContext context)
+	{
+		if (!ArgUtility.TryGet(query, 1, out string location, out string error, false))
+		{
+			return GSQHelper.ErrorResult(query, error);
+		}
+
+		GameLocation loc = GSQHelper.GetLocation(location, context.Location);
+
+		if (loc == null)
+		{
+			return GSQHelper.ErrorResult(query, $"Could not find location matching provided location name \"{location}\"");
+		}
+		
+
+		if (!ArgUtility.TryGet(query, 2, out string weather, out error, false))
+		{
+			return GSQHelper.ErrorResult(query, error);
+		}
+
+		List<string> weathers = new() { weather };
+
+		ArgUtility.TryGetOptionalRemainder(query, 3, out string additionalWeathers);
+
+		if (additionalWeathers != null)
+		{
+			weathers.AddRange(ArgUtility.SplitBySpace(additionalWeathers));
+		}
+
+		return weathers.Contains(loc.GetWeather().WeatherForTomorrow);
+	}
+}
