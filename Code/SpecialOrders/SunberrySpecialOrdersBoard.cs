4e40706839be4c9012082412399a05c165b18573
sophiesalacia <sophiesalacia@users.noreply.github.com>
2024-11-30 13:51:39 -0500

    moved hook to cp patch

--- Code/SpecialOrders/SunberrySpecialOrdersBoard.cs
+++ Code/SpecialOrders/SunberrySpecialOrdersBoard.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
 using StardewValley.SpecialOrders;
@@ -14,31 +15,9 @@ internal class SunberrySpecialOrdersBoard : SpecialOrdersBoard
 	{
 		Globals.EventHelper.GameLoop.GameLaunched += RegisterTileAction;
 		Globals.EventHelper.GameLoop.DayStarted += UpdateSpecialOrdersOnWeekStart;
-		Globals.EventHelper.Player.Warped += UpdateSpecialOrderBoardIfBuilt;
 	}
 
-	private static void UpdateSpecialOrderBoardIfBuilt(object sender, StardewModdingAPI.Events.WarpedEventArgs e)
-	{
-		if (!(Game1.currentLocation.Name.Equals("Custom_SBV_SunberryVillage") || Game1.currentLocation.Name.Equals("Custom_SBV_TwilightFestival")) || !Game1.player.eventsSeen.Contains("skellady.SBVCP_20031434") && Game1.currentLocation.currentEvent?.id != "skellady.SBVCP_20031434")
-		{
-			return;
-		}
-
-		const string tileSheetId = "spring_town";
-		GameLocation loc = Game1.currentLocation;
-
-		loc.setMapTile(68, 13, 2045, "Buildings", tileSheetId);
-		loc.setMapTile(69, 13, 2046, "Buildings", tileSheetId);
-		loc.setMapTile(70, 13, 2047, "Buildings", tileSheetId);
-		loc.setTileProperty(68, 13, "Buildings", "Action", "SunberryTeam.SBVSMAPI_SpecialOrdersBoard");
-		loc.setTileProperty(69, 13, "Buildings", "Action", "SunberryTeam.SBVSMAPI_SpecialOrdersBoard");
-		loc.setTileProperty(70, 13, "Buildings", "Action", "SunberryTeam.SBVSMAPI_SpecialOrdersBoard");
-		loc.setMapTile(68, 12, 2013, "Front", tileSheetId);
-		loc.setMapTile(69, 12, 2014, "Front", tileSheetId);
-		loc.setMapTile(70, 12, 2015, "Front", tileSheetId);
-	}
-
-	private static void UpdateSpecialOrdersOnWeekStart(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
+	private static void UpdateSpecialOrdersOnWeekStart(object sender, DayStartedEventArgs e)
 	{
 		if (Game1.IsMasterGame && (Game1.dayOfMonth == 1 || Game1.dayOfMonth == 8 || Game1.dayOfMonth == 15 || Game1.dayOfMonth == 22))
 		{
@@ -46,7 +25,7 @@ internal class SunberrySpecialOrdersBoard : SpecialOrdersBoard
 		}
 	}
 
-	private static void RegisterTileAction(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+	private static void RegisterTileAction(object sender, GameLaunchedEventArgs e)
 	{
 		GameLocation.RegisterTileAction("SunberryTeam.SBVSMAPI_SpecialOrdersBoard", OpenSpecialOrdersMenu);
 	}
